f313f857a53191fe847c80aba3e21269a079de37
mohanrajrc <mohanrajksrct@gmail.com>
2020-09-03 11:35:24 +0530

    Custom Queue Implementation using Arrays and Stacks

---
+++ src/com/rcmcode/algorithms/queues/CustomArrayQueue.java
@@ -0,0 +1,41 @@
+package com.rcmcode.algorithms.queues;
+
+import java.util.Arrays;
+
+public class CustomArrayQueue {
+
+    private int[] store;
+    private int front;
+    private int rear;
+    private int count;
+
+
+    public CustomArrayQueue() {
+        store = new int[5];
+    }
+
+    public void enqueue(int item) {
+        if(count == store.length) throw new IllegalStateException();
+        store[rear] = item;
+        rear = (rear+1)%store.length;
+        count++;
+    }
+
+    public int dequeue() {
+        if(isEmpty()) throw new IllegalStateException();
+        int item = store[front];
+        store[front] = 0;
+        front = (front+1)%store.length;
+        count--;
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(store);
+    }
+}
